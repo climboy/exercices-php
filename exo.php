02c6513de684f8c3b6216c63780f43f45b314888
thomaslebg70 <thomas.clavier70@yahoo.fr>
2017-02-02 14:39:59 +0100

    Exercices-PHP

---
+++ exo.php
@@ -0,0 +1,106 @@
+<?php
+function adult($age)
+{
+  if ($age>=18) {
+    echo "vous etes majeur"."<br />";
+  }
+  else {
+    echo "vous etes un gamin"."<br />";
+  }
+}
+
+function compte()
+{
+  for ($i=0; $i<=10 ; $i++) {
+  echo "mon nombre est $i "."<br />";
+  }
+}
+
+
+
+
+
+
+
+
+
+
+adult(66);
+adult(6);
+compte(8);
+
+
+
+
+
+
+
+ ?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
